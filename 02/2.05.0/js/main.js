a718b10ce76b14a91faab44974e9b9cc1f4afbe0
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-04-28 23:11:26 +0700

    Section 2, Lecture 12

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,19 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 500)
+	.attr("height", 500);
+
+var circle = svg.append("circle")
+	.attr("cx", 70)
+	.attr("cy", 100)
+	.attr("r", 50)
+    .attr("fill", "red");
+    
+var text = svg.append("text")
+	.attr("x", 150)
+	.attr("y", 100)
+    .attr("fill", "red")
+    .text("This is a circle")
