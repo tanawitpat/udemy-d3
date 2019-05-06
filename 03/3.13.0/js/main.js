f6498eb1e8d2c81194bcb8ab1a72a9e379f18444
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-05-06 22:46:45 +0700

    Section 3, Lecture 28

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -4,3 +4,76 @@
 *    Project 1 - Star Break Coffee
 */
 
+var margin = { left:80, right:20, top:50, bottom:100 };
+
+var width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var g = d3.select("#chart-area")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Month")
+
+g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (height / 2))
+    .attr("y", -60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Revenue")
+
+d3.json("data/revenues.json").then(function(data){
+    console.log(data)
+    data.forEach(function(d){
+        d.revenue = +d.revenue
+    })
+    
+    var x = d3.scaleBand()
+        .domain(data.map(function(d){ return d.month }))
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3)
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){
+            return d.revenue
+        })])
+        .range([height, 0])
+
+    var xAxisCall = d3.axisBottom(x)
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxisCall)
+        .selectAll("text")
+            .attr("y", "10")
+            .attr("x", "-5")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-40)")
+
+    var yAxisCall = d3.axisLeft(y).ticks(3)
+    g.append("g")
+        .attr("class", "y-axis")
+        .call(yAxisCall)    
+
+    var rects = g.selectAll("rect").data(data)
+
+    rects.enter()
+        .append("rect")
+        .attr("y", function(d){ return y(d.revenue) })
+        .attr("x", function(d){ return x(d.month) })
+        .attr("width", x.bandwidth)
+        .attr("height", function(d){ return height - y(d.revenue) })
+        .attr("fill", "grey")
+})
