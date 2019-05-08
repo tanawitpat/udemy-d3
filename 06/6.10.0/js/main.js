e9ef2f01978b7d1b3107296bf6f938d409a4da75
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-05-08 21:59:50 +0700

    Section 6, Lecture 56

--- 06/6.10.0/js/main.js
+++ 06/6.10.0/js/main.js
@@ -6,122 +6,173 @@
 
 var margin = { left:80, right:100, top:50, bottom:100 },
     height = 500 - margin.top - margin.bottom, 
-    width = 800 - margin.left - margin.right;
+    width = 800 - margin.left - margin.right
 
-var svg = d3.select("#chart-area").append("svg")
+var svg = d3.select("#chart-area")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("height", height + margin.top + margin.bottom)
 
 var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + 
-        ", " + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
-// Time parser for x-scale
-var parseTime = d3.timeParse("%Y");
-// For tooltip
-var bisectDate = d3.bisector(function(d) { return d.year; }).left;
+var t = () => { return d3.transition().duration(1000) }
 
-// Scales
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+var parseTime = d3.timeParse("%d/%m/%Y")
+var sliderTime = d3.timeFormat("%d/%m/%Y")
+var bisectDate = d3.bisector(function(d) { return d.date }).left
 
-// Axis generators
+g.append("path")
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "grey")
+    .attr("stroke-width", "3px")
+
+var xLabel = g.append("text")
+    .attr("class", "x axisLabel")
+    .attr("y", height + 50)
+    .attr("x", width / 2)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Time")
+
+var yLabel = g.append("text")
+    .attr("class", "y axisLabel")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -60)
+    .attr("x", -170)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Price (USD)")
+
+var x = d3.scaleTime().range([0, width])
+var y = d3.scaleLinear().range([height, 0])
+
 var xAxisCall = d3.axisBottom()
-var yAxisCall = d3.axisLeft()
-    .ticks(6)
-    .tickFormat(function(d) { return parseInt(d / 1000) + "k"; });
-
-// Axis groups
+    .ticks(4)
 var xAxis = g.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")");
+    .attr("transform", "translate(0," + height +")")
+
+var yAxisCall = d3.axisLeft()
 var yAxis = g.append("g")
     .attr("class", "y axis")
-    
-// Y-Axis label
-yAxis.append("text")
-    .attr("class", "axis-title")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .attr("fill", "#5D6971")
-    .text("Population)");
 
-// Line path generator
-var line = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.value); });
+$("#coin-select").on("change", update)
+$("#var-select").on("change", update)
 
-d3.json("data/example.json").then(function(data) {
-    // Data cleaning
-    data.forEach(function(d) {
-        d.year = parseTime(d.year);
-        d.value = +d.value;
-    });
+$("#date-slider").slider({
+    range: true,
+    max: parseTime("31/10/2017").getTime(),
+    min: parseTime("12/5/2013").getTime(),
+    step: 86400000,
+    values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
+    slide: (event, ui) => {
+        $("#dateLabel1").text(sliderTime(new Date(ui.values[0])))
+        $("#dateLabel2").text(sliderTime(new Date(ui.values[1])))
+        update()
+    }
+})
 
-    // Set scale domains
-    x.domain(d3.extent(data, function(d) { return d.year; }));
-    y.domain([d3.min(data, function(d) { return d.value; }) / 1.005, 
-        d3.max(data, function(d) { return d.value; }) * 1.005]);
+d3.json("data/coins.json").then(function(data){
+    filteredData = {}
+    for (var coin in data) {
+        if (!data.hasOwnProperty(coin)) {
+            continue
+        }
+        filteredData[coin] = data[coin].filter(d => {
+            return !(d["price_usd"] == null)
+        })
+        filteredData[coin].forEach(d => {
+            d["price_usd"] = +d["price_usd"]
+            d["24h_vol"] = +d["24h_vol"]
+            d["market_cap"] = +d["market_cap"]
+            d["date"] = parseTime(d["date"])
+        })
+    }
+    update()
+})
 
-    // Generate axes once scales have been set
-    xAxis.call(xAxisCall.scale(x))
-    yAxis.call(yAxisCall.scale(y))
+function update() {
+    var coin = $("#coin-select").val(),
+        yValue = $("#var-select").val(),
+        sliderValues = $("#date-slider").slider("values")
 
-    // Add line to chart
-    g.append("path")
-        .attr("class", "line")
-        .attr("fill", "none")
-        .attr("stroke", "grey")
-        .attr("stroke-with", "3px")
-        .attr("d", line(data));
+    var dataTimeFiltered = filteredData[coin].filter(function(d){
+        return ((d.date >= sliderValues[0]) && (d.date <= sliderValues[1]))
+    })
 
-    /******************************** Tooltip Code ********************************/
+    x.domain(d3.extent(dataTimeFiltered, function(d){ return d.date }))
+    y.domain([d3.min(dataTimeFiltered, function(d){ return d[yValue] }) / 1.005, 
+        d3.max(dataTimeFiltered, function(d){ return d[yValue] }) * 1.005])
+
+    var formatSi = d3.format(".2s")
+    function formatAbbreviation(x) {
+        var s = formatSi(x)
+        switch (s[s.length - 1]) {
+            case "G": return s.slice(0, -1) + "B"
+            case "k": return s.slice(0, -1) + "K"
+        }
+        return s
+    }
+
+    xAxisCall.scale(x)
+    xAxis.transition(t()).call(xAxisCall)
+    yAxisCall.scale(y)
+    yAxis.transition(t()).call(yAxisCall.tickFormat(formatAbbreviation))
+
+    d3.select(".focus").remove()
+    d3.select(".overlay").remove()
 
     var focus = g.append("g")
         .attr("class", "focus")
-        .style("display", "none");
+        .style("display", "none")
 
     focus.append("line")
         .attr("class", "x-hover-line hover-line")
         .attr("y1", 0)
-        .attr("y2", height);
-
+        .attr("y2", height)
     focus.append("line")
         .attr("class", "y-hover-line hover-line")
         .attr("x1", 0)
-        .attr("x2", width);
-
+        .attr("x2", width)
     focus.append("circle")
-        .attr("r", 7.5);
-
+        .attr("r", 5)
     focus.append("text")
         .attr("x", 15)
-        .attr("dy", ".31em");
+        .attr("dy", ".31em")
 
-    g.append("rect")
+    svg.append("rect")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .attr("class", "overlay")
         .attr("width", width)
         .attr("height", height)
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
+        .on("mouseover", function() { focus.style("display", null) })
+        .on("mouseout", function() { focus.style("display", "none") })
+        .on("mousemove", mousemove)
 
     function mousemove() {
         var x0 = x.invert(d3.mouse(this)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.year > d1.year - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
-        focus.select("text").text(d.value);
-        focus.select(".x-hover-line").attr("y2", height - y(d.value));
-        focus.select(".y-hover-line").attr("x2", -x(d.year));
+            i = bisectDate(dataTimeFiltered, x0, 1),
+            d0 = dataTimeFiltered[i - 1],
+            d1 = dataTimeFiltered[i],
+            d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0
+        focus.attr("transform", "translate(" + x(d.date) + "," + y(d[yValue]) + ")")
+        focus.select("text").text(function() { return d3.format("$,")(d[yValue].toFixed(2)) })
+        focus.select(".x-hover-line").attr("y2", height - y(d[yValue]))
+        focus.select(".y-hover-line").attr("x2", -x(d.date))
     }
 
+    line = d3.line()
+        .x(function(d){ return x(d.date) })
+        .y(function(d){ return y(d[yValue]) })
 
-    /******************************** Tooltip Code ********************************/
+    g.select(".line")
+        .transition(t)
+        .attr("d", line(dataTimeFiltered))
 
-});
-
+    var newText = (yValue == "price_usd") ? "Price (USD)" :
+        ((yValue == "market_cap") ?  "Market Capitalization (USD)" : "24 Hour Trading Volume (USD)")
+    
+    yLabel.text(newText)
+}
